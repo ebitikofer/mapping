333caad217d21b883fe338e86d3e32789a340b4a
Eric Bitikofer <e.bitikofer@gmail.com>
2016-11-28 23:19:01 -0500

    working

--- map.cpp
+++ map.cpp
@@ -15,9 +15,6 @@ void map::input(){
 
   for(int i = 0; i < n; i++){
     vector< int > empty;
-//    for(int j = 0; j < n; j++){
-//      empty.push_back(0);
-//    }
     path.push_back(empty);
   }
 
@@ -56,49 +53,48 @@ void map::input(){
 
 void map::routeFinder(){
 
-  //vector< int > checker;
   int adder;
 
-	adder = 0;
+	adder = 0; //distance from last location to current (starts at 0)
 
-  for(int i = 0; i < n; i++){
+  for(int i = 0; i < n; i++){ //long loop walks through row choosing shortest route each time (Breadth First)
 
     vector< int > checker;
-    checker = distance[i];
+    checker = distance[i]; //sets the checker to current row in matrix
 
-    for(int j = i; j < n; j++){
-      checker[j] += adder;
+    for(int j = i; j < n; j++){ //starts at current location and goes through the ones we haven't reached yet
+      checker[j] += adder; //adds the shortest distance from first to current to each location that we havent reached yet
     }
 
-    for(int j = i; j < n; j++){
-      if(checker[j] < weight[j] && checker[j] != 0){
-        weight[j] = checker[j];
-        path[j].push_back(i);
+    for(int j = i; j < n; j++){ //starts at current location and goes through the ones we haven't reached yet
+      if(checker[j] < weight[j]){// && checker[j] != 0){ //if the compination tried is shorter than the direct route then swap as long a the chcker route isnt zero which would mean it didnt exist
+        if(checker[j] != 0){
+          weight[j] = checker[j];
+        }
+        path[j].push_back(i); //push back path taken value
       }
     }
 
-    //found[i] = true;
-	  //repeat till fall off and found all true
+    adder = weight[i+1]; //change the add
 
-    adder = weight[i+1];
+  }
 
+  for(int i = 0; i < n; i++){
+    int temp;
+    temp = path[i][path[i].size()-1]; //set temp to last value in path
+    if(temp != i){ //if last value doesnt equal the final destination, add it to the end (it doesn't for all but first path where the start and destination are the same)
+      path[i].push_back(i); //push back the destination value
+    }
   }
 
 }
 
 void map::output(){
 
-  //for(int i = 0; i < n; i++){
-  //  cout << weight[i] << endl;//name[i] << endl;
-  //}
-
   for(int i = 0; i < path[n-1].size(); i++){
     cout << name[path[n-1][i]] << " ";
   }
 
   cout << weight[n-1] << endl;
-
-  //loop
-  //print names according to path then weight
 
 }
